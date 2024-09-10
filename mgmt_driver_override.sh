8b039dd0e90b6ed2247a3a667955b2d4bf275e50
mhermsdorferf5 <m.hermsdorfer@f5.com>
2024-09-10 14:00:51 -0400

    tweaked script to remove newlines and added a simplified offline install in readme.

--- mgmt_driver_override.sh
+++ mgmt_driver_override.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
-
+# This script will override the magement interface driver at boot time, should be called from a systemd service.
 logger -p local0.notice "mgmt_driver_override: Running MX5/6 Mgmt Driver Override Script"
 echo "mgmt_driver_override: starting script run"
-
 driver="mlx5_core"
-
 pci_addr=$(lspci | grep -m1 Mellanox | awk '{ print "0000:"$1 }')
 pci_path="/sys/bus/pci/devices/${pci_addr}"
-
-
 # Add driver override entry
 if [[ -d $pci_path ]]; then
   echo "mgmt_driver_override: Writing driver to ${pci_path}/driver_override"
@@ -19,7 +15,6 @@ else
   logger -p local0.err "mgmt_driver_override: ERROR: PCI device path ($pci_path) not found."
   exit 1
 fi
-
 # forcibly probe that PCI slot
 echo "mgmt_driver_override: Forcing driver probe of $pci_addr"
 logger -p local0.notice "mgmt_driver_override: Forcing driver probe of ${pci_addr}"
